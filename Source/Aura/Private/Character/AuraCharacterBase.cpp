aac7cc28de0d09dd01c1a47bfd5e32c69997c0be
dhtnekfzz <dhtnekfzz@gnu.ac.kr>
2024-03-15 14:32:48 +0900

    Animation Blueprints

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -5,15 +5,19 @@
 
 AAuraCharacterBase::AAuraCharacterBase()
 {
- 	
 	PrimaryActorTick.bCanEverTick = false;
+	
+	Weapon=CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 
 }
 
 void AAuraCharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+
 }
 
 
