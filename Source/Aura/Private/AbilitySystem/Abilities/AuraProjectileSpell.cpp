6aee0b3003328871b1b4a9a86472e91457a01aa6
dhtnekfzz <dhtnekfzz@gnu.ac.kr>
2024-08-07 21:43:53 +0900

    Ability Damage

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -46,7 +46,9 @@ void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileLocation)
 		const FGameplayEffectSpecHandle SpecHandle=SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), SourceASC->MakeEffectContext());
 
 		const FAuraGameplayTags GameplayTags=FAuraGameplayTags::Get();
-		UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(SpecHandle, GameplayTags.Damage, 50.f);
+		const float ScaledDamage=Damage.GetValueAtLevel(GetAbilityLevel());
+		
+		UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(SpecHandle, GameplayTags.Damage, ScaledDamage);
 		Projectile->DamageEffectSpecHandle=SpecHandle;
 		
 		Projectile->FinishSpawning(SpawnTransform);
