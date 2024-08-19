349c9bd5b5030b28e07ec9e95fc49758ea1d5a0c
dhtnekfzz <dhtnekfzz@gnu.ac.kr>
2024-08-19 18:32:55 +0900

    Playing the Ranged Attack Montage

--- Source/Aura/Private/AbilitySystem/Abilities/AuraDamageGameplayAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraDamageGameplayAbility.cpp
@@ -18,3 +18,12 @@ void UAuraDamageGameplayAbility::CauseDamage(AActor* TargetActor)
 	GetAbilitySystemComponentFromActorInfo()->ApplyGameplayEffectSpecToTarget(*DamageSpecHandle.Data.Get(), UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor));
 	
 }
+
+FTaggedMontage UAuraDamageGameplayAbility::GetRandomTaggedMontageFromArray(const TArray<FTaggedMontage>& TaggedMontages) const
+{
+	if(TaggedMontages.IsEmpty()) return FTaggedMontage();
+	const int32 Selection=FMath::RandRange(0, TaggedMontages.Num()-1);
+	return TaggedMontages[Selection];
+	
+}
+
