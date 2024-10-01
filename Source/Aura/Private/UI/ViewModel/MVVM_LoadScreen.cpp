0fda98d0853d5aafdd55c31a30c816639f197a5c
dhtnekfzz <dhtnekfzz@gnu.ac.kr>
2024-10-01 20:20:51 +0900

    Showing Player Level in Load Screen

--- Source/Aura/Private/UI/ViewModel/MVVM_LoadScreen.cpp
+++ Source/Aura/Private/UI/ViewModel/MVVM_LoadScreen.cpp
@@ -40,6 +40,7 @@ void UMVVM_LoadScreen::NewSlotButtonPressed(int32 Slot, const FString& EnteredNa
 
 	LoadSlots[Slot]->SetMapName(AuraGameMode->DefaultMapName);
 	LoadSlots[Slot]->SetPlayerName(EnteredName);
+	LoadSlots[Slot]->SetPlayerLevel(1);
 	LoadSlots[Slot]->SlotStatus=Taken;
 	LoadSlots[Slot]->PlayerStartTag=AuraGameMode->DefaultPlayerStartTag;
 
@@ -118,6 +119,7 @@ void UMVVM_LoadScreen::LoadData()
 		LoadSlot.Value->SlotStatus=SaveSlotStatus;
 		LoadSlot.Value->InitializeSlot();
 		LoadSlot.Value->PlayerStartTag=SaveObject->PlayerStartTag;
+		LoadSlot.Value->SetPlayerLevel(SaveObject->PlayerLevel);
 		
 	}
 }
