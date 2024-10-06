a415fcb79926b29b3fa1d0adaa016bb17974551b
dhtnekfzz <dhtnekfzz@gnu.ac.kr>
2024-10-06 20:21:05 +0900

    Highlighting Non-Enemies

--- Source/Aura/Public/Checkpoint/Checkpoint.h
+++ Source/Aura/Public/Checkpoint/Checkpoint.h
@@ -36,9 +36,10 @@ protected:
 
 	void HandleGlowEffects();
 
-private:
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	TObjectPtr<UStaticMeshComponent> CheckpointMesh;
+
+private:
 
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<USphereComponent> Sphere;
